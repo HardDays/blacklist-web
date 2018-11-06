7eeb8b913c719aeeb145f56745a03e782f936da9
unknown <snolly.gv@gmail.com>
2018-11-06 17:04:49 +0300

    fix package

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const fallback = require('express-history-api-fallback');
 const express = require('express');
+const path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-const root = __dirname + '/dist';
+const root = __dirname + '/dist/BlackList';
 app.use(express.static(root));
 //app.use(fallback('index.html', { root }))
 
 // Start the app by listening on the default
 // Heroku port
 app.get('*', function(req, res) {
-  res.sendfile('./dist/index.html')
+  res.sendfile('./dist/BlackList/index.html')
 })
-var port = process.env.PORT || 8000
+var port = process.env.PORT || 8080
 app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
