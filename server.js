99a273b056a81773953c3c5e2591e47c7905da0c
kparamonov <prmnr2k@gmail.com>
2020-11-19 22:00:45 +0300

    fix server js

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-const root = __dirname + '/dist/BlackList';
+const root = __dirname + '/dist';
 app.use(express.static(root));
 //app.use(fallback('index.html', { root }))
 
 // Start the app by listening on the default
 // Heroku port
 app.get('*', function(req, res) {
-  res.sendfile('./dist/BlackList/index.html')
+  res.sendfile('./dist/index.html')
 })
 var port = process.env.PORT || 8080
 app.listen(port, function(){
